d7747f343c6d9e1a7c8d6018b873d7c186725fc0
bhaden94 <bhaden94@yahoo.com>
2020-11-11 11:12:45 -0800

    add label to theme switcher and some css

--- src/components/theme-switcher/ThemeSwitcher.tsx
+++ src/components/theme-switcher/ThemeSwitcher.tsx
@@ -3,20 +3,23 @@ import { useChangeTheme } from '../../themes';
 import useTheme from '@material-ui/core/styles/useTheme';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Switch from '@material-ui/core/Switch/Switch';
+import { Typography } from '@material-ui/core';
 
-// const useStyles = makeStyles({
-//   root: {
-//   },
-// });
+const useStyles = makeStyles({
+  pad: {
+    paddingLeft: '16px'
+  },
+});
 
 const ThemeSwitcher: React.FC = ()=>{
-  // const classes = useStyles();
+  const classes = useStyles();
   const theme = useTheme();
   const changeTheme = useChangeTheme();
   const checked = theme.palette.type === 'dark'
 
   return (
-    <div>
+    <div className={classes.pad}>
+      <Typography color='textSecondary' display='inline'>Dark Mode</Typography>
       <Switch
         checked={checked}
         onChange={changeTheme}
@@ -24,6 +27,7 @@ const ThemeSwitcher: React.FC = ()=>{
         name="checkedB"
         inputProps={{ 'aria-label': 'primary checkbox' }}
       />
+      
     </div>
   )
 }
