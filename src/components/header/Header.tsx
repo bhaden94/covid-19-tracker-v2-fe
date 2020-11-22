5d5dbfd08c5d98ef2629d919c63e351a6c544d3b
bhaden94 <bhaden94@yahoo.com>
2020-11-21 19:22:28 -0800

    header ready for site info page change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router';
 
 const useStyles = makeStyles(() => ({
     header: {
@@ -12,8 +15,15 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
-const Header = () => {
+const Header = ({ location }: RouteComponentProps) => {
     const classes = useStyles();
+    const [headerText, setHeaderText] = useState<string>('Covid-19 Tracker')
+
+    React.useEffect(() => {
+        location.pathname === '/site_info' ?
+            setHeaderText('Site Information') :
+            setHeaderText('Covid-19 Tracker')
+    }, [location])
 
     return (
         <Typography
@@ -22,9 +32,9 @@ const Header = () => {
             align='center'
             className={classes.header}
         >
-            Covid-19 Tracker
+            {headerText}
         </Typography>
     );
 }
 
-export default Header;
+export default withRouter(Header);
